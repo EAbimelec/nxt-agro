8fa44be49f9bdcc86b53345d51b6f329f7796230
Abimelec <abimelec70.arenas@gmail.com>
2021-12-14 18:06:10 -0600

    Arregla error directorios
    No habia incluido los directorios correctamente por lo que para git era
    como si los hubiera borrado

---
+++ w01/d1/lotide/assertEqual.js
@@ -0,0 +1,16 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  
+  if (actual === expected) {
+    console.log("✅" + actual + " === " + expected);
+
+  } else {
+    console.log("⚠️" + actual + " !== " + expected);
+
+  }
+
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
